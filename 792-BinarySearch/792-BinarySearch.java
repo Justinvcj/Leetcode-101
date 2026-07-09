5a0fa259a12cf28e361192d12675c31b7006cb9b
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-09 09:14:54 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:14:57 AM
+class Solution {
+    public int search(int[] arr, int target) {
+    int left=0;
+    int right=arr.length-1;
+    while(left<=right){
+        int mid=(left+right)/2;
+        if(arr[mid]==target){
+            return mid;
+        }else if(arr[mid]<target){
+             left=mid+1;
+        }else{
+            right=mid-1;
+        }
+    }
+    return -1;
+    }
+}
