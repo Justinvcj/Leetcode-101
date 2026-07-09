1348b2915385c2ef904e6c9402a183f15b52d3a3
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-09 09:17:46 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:17:49 AM
+class Solution {
+    public boolean isPalindrome(int x) {
+    if(x<0) return false;
+        int temp=x;
+        int rev=0;
+        while(x!=0)
+        {
+        int digit=x%10;
+        rev=rev*10+digit;
+        x/=10;
+        }
+     return temp==rev ;       
+    }
+}
