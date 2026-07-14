07260cbc634921ebacc701f9b15e5e271acb9c2d
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-14 11:31:27 +0530

    File updated at 7/14/2026, 11:31:36 AM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
@@ -1,17 +1,17 @@
-// Last updated: 7/9/2026, 9:15:11 AM
-class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-    int maxsum=0;
-    int n=nums.length;
-    int sum=0;
-    for(int i=0;i<k;i++){
-        sum+=nums[i];
-    } 
-    maxsum=sum;
-    for(int i=1;i<n-k+1;i++){
-        sum=sum-nums[i-1]+nums[i+k-1];
-        maxsum=Math.max(maxsum,sum);
-    }   
-    return (double)maxsum/k;
-    }
-}
+// Last updated: 7/14/2026, 11:31:36 AM
+1class Solution {
+2    public double findMaxAverage(int[] nums, int k) {
+3    int maxsum=0;
+4    int n=nums.length;
+5    int sum=0;
+6    for(int i=0;i<k;i++){
+7        sum+=nums[i];
+8    } 
+9    maxsum=sum;
+10    for(int i=1;i<n-k+1;i++){
+11        sum=sum-nums[i-1]+nums[i+k-1];
+12        maxsum=Math.max(maxsum,sum);
+13    }   
+14    return (double)maxsum/k;
+15    }
+16}
