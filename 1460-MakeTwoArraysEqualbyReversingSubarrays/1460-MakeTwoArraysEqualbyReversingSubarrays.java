7ab4e36d554972304b97ee2bb677c9dde6ad3fb4
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-27 13:42:16 +0530

    File updated at 7/27/2026, 1:42:17 PM

--- 1460-MakeTwoArraysEqualbyReversingSubarrays/1460-MakeTwoArraysEqualbyReversingSubarrays.java
+++ 1460-MakeTwoArraysEqualbyReversingSubarrays/1460-MakeTwoArraysEqualbyReversingSubarrays.java
@@ -1,16 +1,16 @@
-// Last updated: 7/27/2026, 1:39:03 PM
+// Last updated: 7/27/2026, 1:42:17 PM
 1class Solution {
 2    public int countConsistentStrings(String s, String[] arr) {
-3    List<Character>list=new ArrayList<>();
+3    boolean[] freq = new boolean[26];
 4    for(char ch:s.toCharArray()){
-5        list.add(ch);
+5        freq[ch-'a']=true;
 6    } 
 7    int count=0;
 8    for(String word:arr){
 9        boolean ok = true; 
 10        for(char ch:word.toCharArray()){
 11
-12        if(!list.contains(ch)){
+12        if(!freq[ch-'a']){
 13            ok=false;
 14            break;
 15        }
