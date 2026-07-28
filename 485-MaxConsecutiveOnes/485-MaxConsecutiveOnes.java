f7fb8bec2c16e6e283cea07637f0100cc6e98bb1
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-28 14:07:39 +0530

    File updated at 7/28/2026, 2:07:38 PM

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.java
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.java
@@ -1,25 +1,24 @@
-// Last updated: 7/28/2026, 2:04:38 PM
+// Last updated: 7/28/2026, 2:07:38 PM
 1class Solution {
 2    public int minimumRecolors(String s, int k) {
 3    int count=0;
 4    int max=0;
-5    int black=0;
-6    for(int i=0;i<k;i++){
-7        if(s.charAt(i)=='W'){
-8            count++;
-9        }
-10    }  
-11    max=count;
-12    for(int i=k;i<s.length();i++){
-13        if(s.charAt(i)=='W'){
-14            count++;
-15        }
-16
-17        if(s.charAt(i-k)=='W'){
-18            count--;
-19        }
-20        max=Math.min(max,count);
-21    }
-22    return max; 
-23    }
-24}
+5    for(int i=0;i<k;i++){
+6        if(s.charAt(i)=='W'){
+7            count++;
+8        }
+9    }  
+10    max=count;
+11    for(int i=k;i<s.length();i++){
+12        if(s.charAt(i)=='W'){
+13            count++;
+14        }
+15
+16        if(s.charAt(i-k)=='W'){
+17            count--;
+18        }
+19        max=Math.min(max,count);
+20    }
+21    return max; 
+22    }
+23}
