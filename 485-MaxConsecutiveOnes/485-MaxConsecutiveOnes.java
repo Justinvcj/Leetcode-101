e735dd5f2ca43274c54ae4763a41069e2fe96050
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-28 14:04:38 +0530

    File updated at 7/28/2026, 2:04:38 PM

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.java
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.java
@@ -1,19 +1,25 @@
-// Last updated: 7/9/2026, 9:15:21 AM
-class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
-    int count=0;
-    int max=0;
-    for(int i=0;i<nums.length;i++){
-        if(nums[i]==1){
-            count++;
-            if(count>max){
-                max=count;
-            }
-        }else {
-          
-            count=0;
-        }
-    }
-    return Math.max(max,count); 
-    }
-}
+// Last updated: 7/28/2026, 2:04:38 PM
+1class Solution {
+2    public int minimumRecolors(String s, int k) {
+3    int count=0;
+4    int max=0;
+5    int black=0;
+6    for(int i=0;i<k;i++){
+7        if(s.charAt(i)=='W'){
+8            count++;
+9        }
+10    }  
+11    max=count;
+12    for(int i=k;i<s.length();i++){
+13        if(s.charAt(i)=='W'){
+14            count++;
+15        }
+16
+17        if(s.charAt(i-k)=='W'){
+18            count--;
+19        }
+20        max=Math.min(max,count);
+21    }
+22    return max; 
+23    }
+24}
