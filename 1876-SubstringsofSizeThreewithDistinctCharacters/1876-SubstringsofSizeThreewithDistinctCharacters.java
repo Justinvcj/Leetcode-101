f839a2fd10349f6e45ccc681e01cf4e4a7e332ae
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-29 14:07:02 +0530

    File updated at 7/29/2026, 2:07:01 PM

--- 1876-SubstringsofSizeThreewithDistinctCharacters/1876-SubstringsofSizeThreewithDistinctCharacters.java
+++ 1876-SubstringsofSizeThreewithDistinctCharacters/1876-SubstringsofSizeThreewithDistinctCharacters.java
@@ -1,16 +1,18 @@
-// Last updated: 7/28/2026, 2:58:15 PM
+// Last updated: 7/29/2026, 2:07:01 PM
 1class Solution {
-2    public int countGoodSubstrings(String s) {
-3    int count=0;
-4
-5    for(int i=0;i<=s.length()-3;i++){
-6        if(s.charAt(i) != s.charAt(i+1) &&
-7            s.charAt(i) != s.charAt(i+2) &&
-8            s.charAt(i+1) != s.charAt(i+2)){
-9
-10    count++;
-11            }
-12    }
-13     return count;  
+2    public boolean containsNearbyDuplicate(int[] nums, int k) {
+3
+4    HashSet<Integer>set=new HashSet<>();
+5    for(int i=0;i<nums.length;i++){
+6        if(set.contains(nums[i])){
+7            return true;
+8        }
+9        set.add(nums[i]);
+10
+11        if(set.size()>k){
+12            set.remove(nums[i-k]);
+13        }
 14    }
-15}
+15    return false;   
+16    }
+17}
