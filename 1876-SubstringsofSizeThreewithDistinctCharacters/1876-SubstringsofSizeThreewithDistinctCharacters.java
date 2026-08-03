f60a1851aa6f8509d0a8d94440b7a5f8cddeefe7
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-08-03 14:38:00 +0530

    File updated at 8/3/2026, 2:38:04 PM

--- 1876-SubstringsofSizeThreewithDistinctCharacters/1876-SubstringsofSizeThreewithDistinctCharacters.java
+++ 1876-SubstringsofSizeThreewithDistinctCharacters/1876-SubstringsofSizeThreewithDistinctCharacters.java
@@ -1,16 +1,35 @@
-// Last updated: 8/3/2026, 12:23:07 PM
+// Last updated: 8/3/2026, 2:38:04 PM
 1class Solution {
-2    public int divisorSubstrings(int num, int k) {
-3    int count=0;
-4    String str = String.valueOf(num);
-5    for(int i=0;i<=str.length()-k;i++){
-6        String sub=str.substring(i,i+k);
-7        int val=Integer.parseInt(sub);
-8
-9        if(val!=0 && num %val==0){
-10            count++;
-11        }
-12    }
-13    return count;
+2    public long maximumSubarraySum(int[] nums, int k) {
+3    long sum=0;
+4    long max=0;
+5    HashMap<Integer,Integer>map=new HashMap<>();
+6
+7    for(int i=0;i<k;i++){
+8        sum+=nums[i];
+9        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+10    }
+11    if(map.size()==k){
+12
+13    max=sum;
 14    }
-15}
+15    
+16    for(int i=k;i<nums.length;i++){
+17        sum+=nums[i];
+18        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+19
+20        sum-=nums[i-k];
+21        map.put(nums[i-k],map.getOrDefault(nums[i-k],0)-1);
+22
+23        if(map.get(nums[i-k])==0){
+24            map.remove(nums[i-k]);
+25        }
+26
+27        if (map.size() == k){
+28                max = Math.max(max, sum);
+29        }
+30    }
+31
+32    return max;
+33    }
+34}
