765c793e9314fa3b0e2ee97759c199edfc5ee6e7
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-08-11 15:42:17 +0530

    File updated at 8/11/2026, 3:42:18 PM

--- 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
@@ -1,26 +1,19 @@
-// Last updated: 8/11/2026, 2:54:12 PM
+// Last updated: 8/11/2026, 3:42:18 PM
 1class Solution {
-2    public String[] divideString(String s, int k, char fill) {
-3    int n=s.length();
-4    StringBuilder sb=new StringBuilder();
-5    int x=n%k;
-6    int y=0;
-7    if(x!=0){
-8        y=k-x;
-9        for(int i=0;i<y;i++){
-10            sb.append(fill);
-11        }
-12    } 
-13    for(int i=s.length()-1;i>=0;i--){
-14        sb.append(s.charAt(i));
-15    } 
-16    String full=sb.reverse().toString();
-17    int num = full.length() / k;
-18    String[] result = new String[num];
-19
-20    for(int i=0;i<num;i++){
-21        result[i]=full.substring(i * k, (i + 1) * k);
-22    }
-23    return result;
-24    }
-25}
+2    public String reverseStr(String s, int k) {
+3    char [] arr=s.toCharArray();
+4    int n=arr.length;
+5    for(int start=0;start<n;start+=2*k){
+6        int left=start;
+7        int right=Math.min(start+k-1,n-1);
+8        while (left < right) {
+9                char temp = arr[left];
+10                arr[left] = arr[right];
+11                arr[right] = temp;
+12                left++;
+13                right--;
+14            }
+15    }   
+16    return new String(arr);
+17    }
+18}
