7bc9c297f54bcab69cacbcec45b4ae0799579ab4
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-08-11 14:54:12 +0530

    File updated at 8/11/2026, 2:54:12 PM

--- 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
@@ -1,15 +1,26 @@
-// Last updated: 8/11/2026, 2:16:51 PM
+// Last updated: 8/11/2026, 2:54:12 PM
 1class Solution {
-2    public String thousandSeparator(int n) {
-3    StringBuilder sb=new StringBuilder();
-4    String s=String.valueOf(n);
-5    int k=3;
-6    for(int i=s.length()-1;i>=0;i--){
-7        if(sb.length() > 0 && sb.length() % (k + 1) == k){
-8            sb.append(".");
-9        }
-10        sb.append(s.charAt(i));
-11    } 
-12    return sb.reverse().toString();   
-13    }
-14}
+2    public String[] divideString(String s, int k, char fill) {
+3    int n=s.length();
+4    StringBuilder sb=new StringBuilder();
+5    int x=n%k;
+6    int y=0;
+7    if(x!=0){
+8        y=k-x;
+9        for(int i=0;i<y;i++){
+10            sb.append(fill);
+11        }
+12    } 
+13    for(int i=s.length()-1;i>=0;i--){
+14        sb.append(s.charAt(i));
+15    } 
+16    String full=sb.reverse().toString();
+17    int num = full.length() / k;
+18    String[] result = new String[num];
+19
+20    for(int i=0;i<num;i++){
+21        result[i]=full.substring(i * k, (i + 1) * k);
+22    }
+23    return result;
+24    }
+25}
