989c97a29f2e5e5fb8075a2a356a3646c56ce4f6
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-09-08 10:00:41 +0530

    Create 1431-KidsWithTheGreatestNumberOfCandies

---
+++ 1431-KidsWithTheGreatestNumberOfCandies/1431-KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,22 @@
+// Last updated: 9/8/2026, 10:00:41 AM
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    List<Boolean>list=new ArrayList<>();   
+    int max=0;
+    for(int i=0;i<candies.length;i++){
+        if(candies[i]>max){
+            max=candies[i];
+        }
+    }
+    for(int i=0;i<candies.length;i++){
+        
+        candies[i]+=extraCandies;
+        if(candies[i]>=max){
+            list.add(true);
+        }else{
+            list.add(false);
+        }
+    }
+    return list; 
+    }
+}
