51758230c237c395bf273a849fd1565660dc0ea4
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-09-08 09:59:24 +0530

    Create 2110-NumberOfSmoothDescentPeriodsOfAStock

---
+++ 2110-NumberOfSmoothDescentPeriodsOfAStock/2110-NumberOfSmoothDescentPeriodsOfAStock.java
@@ -0,0 +1,16 @@
+// Last updated: 9/8/2026, 9:59:24 AM
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+    long ans=1;
+    long count=1;
+    for(int i=1;i<prices.length;i++){
+        if(prices[i-1]-prices[i]==1){
+            count++;
+        }else{
+            count=1;
+        }
+        ans+=count;
+    }
+    return ans;
+    }
+}
