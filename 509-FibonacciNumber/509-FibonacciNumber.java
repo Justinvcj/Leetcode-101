49e2f9a93eb65f779caeb9530f3ab6ee84a818a2
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-09-08 10:01:33 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 9/8/2026, 10:01:33 AM
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+     int a=0;
+     int b=1;
+     for(int i=0;i<n;i++){
+        int c=a+b;
+        a=b;
+        b=c;
+     }   
+    
+    return a;
+    }
+    
+}
