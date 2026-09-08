ff5516d81466b916ed0d85c0572c7e99e1d79171
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-09-08 10:01:18 +0530

    Create 1041-RobotBoundedInCircle

---
+++ 1041-RobotBoundedInCircle/1041-RobotBoundedInCircle.java
@@ -0,0 +1,23 @@
+// Last updated: 9/8/2026, 10:01:18 AM
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+    int x=0, y=0;
+    int dir=0;
+    int [][] direction={{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+    for(char c : instructions.toCharArray()){
+        if(c=='G'){
+            x+=direction[dir][0];
+            y+=direction[dir][1];
+
+        }
+        else if(c=='L'){
+            dir=(dir+3)%4;
+        }
+        else if(c=='R'){
+            dir=(dir+1)%4;
+        }
+    }
+    return ((x==0 && y==0) ||(dir !=0)); 
+    }
+}
