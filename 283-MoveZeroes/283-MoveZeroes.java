39bb466d2ead1ee91c27af7ab8934974c89b7bc1
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-09-09 12:34:29 +0530

    File updated at 9/9/2026, 12:34:29 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,17 +1,19 @@
-// Last updated: 7/9/2026, 9:16:04 AM
-class Solution {
-    public void moveZeroes(int[] nums) {
-    int index=0;
-    for(int i=0;i<nums.length;i++){
-        if(nums[i]!=0){
-            nums[index]=nums[i];
-            index++;
-        }
-    }
-    while(index<nums.length){
-        nums[index]=0;
-        index++;
-    }
-
-    }
-}
+// Last updated: 9/9/2026, 12:34:29 PM
+1class Solution {
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3    HashMap<String,List<String>>list=new HashMap<>();
+4    for(String word:strs){
+5    int [] freq=new int[26];
+6    for(char ch:word.toCharArray()){
+7        freq[ch-'a']++;
+8    } 
+9    StringBuilder sb=new StringBuilder();  
+10    for(int count:freq){
+11        sb.append(count).append("#");
+12    }
+13    list.putIfAbsent(sb.toString(),new ArrayList<>());
+14    list.get(sb.toString()).add(word);
+15    }
+16    return new ArrayList<>(list.values());
+17    }
+18}
